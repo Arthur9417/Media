f4f966f2c971f079ac6a21a6b12be715cc6cb6c0
Arthur9417 <arthur.fund@icloud.com>
2025-12-01 22:11:34 +0100

    Create Article.jsx

---
+++ src/components/Article.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ArticleList() {
+  return (
+    <div>
+      <p>Aucun article pour le moment. Ajoutez-en depuis votre code !</p>
+    </div>
+  );
+}
+
+export default ArticleList;
