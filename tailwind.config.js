c077aee28c38d8604840444b0cee455ff6553199
Arthur9417 <arthur.fund@icloud.com>
2025-12-01 22:00:09 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 export default {
-content: ["./index.html", "./src/**/*.{js,jsx}"],
-theme: {
-extend: {},
-},
-plugins: [require('@tailwindcss/typography')],
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [], // AUCUN PLUGIN -> fonctionne sur Vercel
 }
